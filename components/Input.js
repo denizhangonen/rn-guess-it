730316ee7df4e084b99f24524713c59262cb4ab9
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-02-05 19:55:10 +0300

    initial commit

---
+++ components/Input.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+import Colors from "../constants/colors";
+
+const Input = props => {
+  return <TextInput />;
+};
+
+const styles = StyleSheet.create({});
+
+export default Input;
